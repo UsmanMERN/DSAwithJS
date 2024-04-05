53a45385bc66557dbf174b8861ca20e59d47eed7
UsmanMern <mu1363131@gmail.com>
2024-04-06 01:32:34 +0500

    learning

---
+++ 01_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict" // treat all js code as newer version
+
+// console.log(3 + 3)
+// console.log("usman")
+
+// let name = "usman"
+// let age = 21
+// let isLoggedIn = false
+
+// number
+// bigInt
+// string=>""
+// boolean => true/false
+// null
+// undefined
+// symbol => unique
+
+// object
+
+// console.log('first', typeof undefined)
+
+// let isLoggedIn = 9;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// *******************************      operation       ***********************************
+
+let value = 3;
+
+let negValue = -value
+
+console.log('negValue', negValue)
