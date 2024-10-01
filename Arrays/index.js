de5d1e6508bb9a595098d2f96505b2523562dc21
UsmanMern <mu1363131@gmail.com>
2024-10-02 00:08:49 +0500

    working

--- Arrays/index.js
+++ Arrays/index.js
@@ -314,26 +314,222 @@ const myarr = [1, 2, 3, 4, 5, 6, 7, 8]
 // console.log(removeDuplicate([1, 1, 1, 2, 2, 1, 2, 1, 3, 4, , 4, 4]))
 
 
-var maxProfit = function (prices) {
-    let smallValue = prices[0]
-    let largeValue = prices[0]
-    let indexOfSellingPoint = 0
-    let ans = 0
+// var maxProfit = function (prices) {
+//     let smallValue = prices[0]
+//     let largeValue = prices[0]
+//     let indexOfSellingPoint = 0
+//     let ans = 0
 
-    // smallValue = Math.min(...prices)
-    for (let i = 0; i < prices.length; i++) {
-        const element = prices[i];
-        if (element < smallValue) {
-            smallValue = element
-            largeValue = 0
+//     // smallValue = Math.min(...prices)
+//     for (let i = 0; i < prices.length; i++) {
+//         const element = prices[i];
+//         if (element < smallValue) {
+//             smallValue = element
+//             largeValue = 0
+//         }
+//         if (element > largeValue) {
+//             largeValue = element
+//         }
+//         let diff = largeValue - smallValue
+//         ans = diff > ans ? diff : ans
+//     }
+//     return ans
+// };
+
+// console.log('maxProfit([7,1,5,3,6,4]) :>> ', maxProfit([2, 4, 1]));
+
+
+// var maxProduct = function (nums) {
+//     let prod = 1
+//     let element = 1
+//     let currectMin = nums[0]
+//     let currectMax = nums[0]
+//     for (let i = 0; i < nums.length; i++) {
+//         element *= nums[i];
+//         let maxium = Math.max(prod, element)
+//         let minium = Math.min(prod, element)
+//         if (currectMax < maxium) {
+//             currectMax = maxium
+//             prod = currectMax
+//         }
+//         if (currectMin < minium) {
+//             currectMin = minium
+//         }
+//         if (minium === 0) {
+//             prod = 0
+//         }
+//     }
+//     return prod
+// };
+// console.log('maxProfit([7,1,5,3,6,4]) :>> ', maxProduct([-2, -1, -1]));
+
+// var singleNumber = function (nums) {
+//     let myArray = []
+
+//     if (nums.length == 1) {
+//         return nums[0]
+//     }
+//     for (let i = 0; i < nums.length; i++) {
+//         const element = nums[i];
+//         if (!myArray.includes(element)) {
+//             myArray.push(element)
+//         } else {
+//             let delIndex = myArray.indexOf(element)
+//             myArray.splice(delIndex, 1)
+//         }
+//     }
+//     return myArray[0]
+// };
+
+// console.log('maxProfit([7,1,5,3,6,4]) :>> ', singleNumber([2, 2, 1]));
+
+// var longestConsecutive = function (nums) {
+//     let globalArray = []
+//     let sizeOfArray = 0
+
+//     nums.sort((a, b) => a - b)
+//     if (nums.length <= 0) {
+//         return 0
+//     }
+//     for (let i = 0; i < nums.length; i++) {
+//         const element = nums[i];
+//         if (!globalArray.includes(element)) {
+//             globalArray.push(element)
+//         } else {
+//             let delIndex = globalArray.indexOf(element)
+//             globalArray.splice(delIndex, 1)
+//         }
+//     }
+
+//     for (let i = 0; i < nums.length; i++) {
+//         const element = nums[i];
+
+//         if (!globalArray.includes(element - 1)) {
+//             let length = 1
+//             while (globalArray.includes(element + length)) {
+//                 length += 1
+//             }
+//             sizeOfArray = Math.max(length, sizeOfArray)
+//         } else {
+//             let delIndex = globalArray.indexOf(element)
+//             globalArray.splice(delIndex, 1)
+//         }
+//     }
+//     console.log('globalArray :>> ', globalArray);
+//     console.log('globalArray :>> ', globalArray.length);
+//     return sizeOfArray
+// };
+
+// var longestConsecutive = function (nums) {
+//     let longest = 0
+//     if (nums.length <= 0) {
+//         return nums[0]
+//     }
+//     let uniqueArray = new Set(nums);
+//     for (const num of uniqueArray) {
+//         if (!uniqueArray.has(num - 1)) {
+//             let count = 0
+//             while (uniqueArray.has(num + count)) {
+
+//                 longest = Math.max(count, longest)
+//                 count++
+//             }
+//         }
+//     }
+//     return longest + 1
+// }
+
+// console.log('maxProfit([7,1,5,3,6,4]) :>> ', longestConsecutive([9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6]));
+
+// var maxProduct = function (nums) {
+//     if (nums.length <= 0) {
+//         return nums[0]
+//     }
+//     let maxium = nums[0]
+//     let minium = nums[0]
+//     let result = nums[0]
+//     for (let i = 1; i < nums.length; i++) {
+//         let current = nums[i]
+//         let currentMax = Math.max(maxium * current, current, minium * current)
+//         let currentMin = Math.min(maxium * current, current, current * maxium)
+//         maxium = currentMax
+//         minium = currentMin
+//         result = Math.max(result, maxium)
+//     }
+//     return result
+// };
+
+// console.log('maxProfit([7,1,5,3,6,4]) :>> ', maxProduct([3, -1, 4]));
+
+// var removeDuplicates = function (nums) {
+//     let count = 1
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] != nums[j]) {
+//                 nums[j] = nums[i]
+//                 count++
+//             }
+//         }
+//     }
+//     return count
+// };
+
+
+// console.log('maxProfit([7,1,5,3,6,4]) :>> ', removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+
+// let dat = new Date()
+
+// console.log('dat :>> ', dat);
+
+
+// var strStr = function (haystack, needle) {
+//     let index = haystack.indexOf(needle)
+//     return index
+// };
+
+
+// console.log('maxProfit([7,1,5,3,6,4]) :>> ', strStr("sadbutsad", "sad"));
+
+// var mergeTwoLists = function (list1, list2) {
+//     let list = list1.concat(list2)
+//     // let list = new Array(list1, list2).flat()
+
+//     let sortedList = list.sort((a, b) => a - b)
+//     return sortedList
+// }
+// console.log('maxProfit([7,1,5,3,6,4]) :>> ', mergeTwoLists([1, 2, 4], [1, 3, 4]));
+
+// var createCounter = function (init) {
+//     console.log('init initial:>> ', init);
+//     let decrement = () => {
+//         console.log('init decrease:>> ', init);
+//         return ++init
+//     }
+//     let increment = () => {
+//         console.log('init increase:>> ', init);
+//         return ++init
+//     }
+//     let reset = () => {
+//         console.log('init reset:>> ', init);
+//         return init
+//     }
+//     return { decrement, increment, reset }
+// };
+// const counter = createCounter(5)
+// console.log('counter.increment(); :>> ', counter.increment()); // 6
+// console.log('counter.reset() :>> ', counter.reset()); // 5
+// console.log('counter.decrement() :>> ', counter.decrement()); // 4
+
+// // console.log('maxProfit([7,1,5,3,6,4]) :>> ', mergeTwoLists([1, 2, 4], [1, 3, 4]));
+
+var canConstruct = function (ransomNote, magazine) {
+    // for (let i = 0; i < ransomNote.length; i++) {
+    //     const element = ransomNote[i];
+    // }
+    for (const i of ransomNote) {
+        if (i) {
+
         }
-        if (element > largeValue) {
-            largeValue = element
-        }
-        let diff = largeValue - smallValue
-        ans = diff > ans ? diff : ans
     }
-    return ans
 };
-
-console.log('maxProfit([7,1,5,3,6,4]) :>> ', maxProfit([2, 4, 1]));
+console.log('maxProfit([7,1,5,3,6,4]) :>> ', canConstruct("aa", "aab"));
